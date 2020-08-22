672e519643a91adf82ede0ee0b6335dfa7429af1
Anastasiia Slobodskaia <anastasiia.slobodskaia@t-systems.com>
2020-08-22 23:07:24 +0300

    add several functions

--- sandbox/src/main/java/ru/stqa/training/sandbox/Cat.java
+++ sandbox/src/main/java/ru/stqa/training/sandbox/Cat.java
@@ -1,7 +1,29 @@
 package ru.stqa.training.sandbox;
 
 public class Cat {
-	public static void main(String[] args) {
-System.out.println("Meow!");
-	}
+    public static void main(String[] args) {
+        meow("world");
+        meow("user");
+        meow("Anastasiia");
+
+        double l = 5;
+        System.out.println("Площадь квадрата со стороной " + l + " равна " + area(l) + ".");
+
+        double a = 4;
+        double b = 6;
+        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " равна " + area(a, b) + ".");
+
+    }
+
+    public static void meow(String somebody) {
+        System.out.println("Meow, " + somebody + "!");
+    }
+
+    public static double area(double len) {
+        return len * len;
+    }
+
+    public static double area(double a, double b) {
+        return a * b;
+    }
 }
