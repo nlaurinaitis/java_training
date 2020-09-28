c38c8c806475e47356beef11c015be6c19048f80
Anastasiia Slobodskaia <anastasiia.slobodskaia@t-systems.com>
2020-09-28 10:38:04 +0300

    realise possibility to read testdata from json for user and group tests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,11 +1,14 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -15,9 +18,8 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
-    //csv
-/*    @DataProvider
-    public Iterator<Object[]> validGroups() throws IOException {
+    @DataProvider
+    public Iterator<Object[]> validGroupsFromCsv() throws IOException {
         List<Object[]> list = new ArrayList<Object[]>();
         BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")));
         String line = reader.readLine();
@@ -27,11 +29,10 @@ public class GroupCreationTests extends TestBase {
             line = reader.readLine();
         }
         return list.iterator();
-    }*/
+    }
 
-    //xml
     @DataProvider
-    public Iterator<Object[]> validGroups() throws IOException {
+    public Iterator<Object[]> validGroupsFromXml() throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")));
         String xml = "";
         String line = reader.readLine();
@@ -45,7 +46,21 @@ public class GroupCreationTests extends TestBase {
         return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
     }
 
-    @Test (dataProvider = "validGroups")
+    @DataProvider
+    public Iterator<Object[]> validGroupsFromJson() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.json")));
+        String json = "";
+        String line = reader.readLine();
+        while (line != null) {
+            json += line;
+            line = reader.readLine();
+        }
+        Gson gson = new Gson();
+        List <GroupData> groups = gson.fromJson(json, new TypeToken<List<GroupData>>(){}.getType());
+        return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+    }
+
+    @Test (dataProvider = "validGroupsFromJson")
     public void testGroupCreation(GroupData group) throws Exception {
             app.goTo().groupPage();
             Groups before = app.group().all();
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/UserCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/UserCreationTests.java
@@ -1,5 +1,7 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -21,9 +23,9 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class UserCreationTests extends TestBase {
 
-    //csv
-/*    @DataProvider
-    public Iterator<Object[]> validUsers() throws IOException {
+
+    @DataProvider
+    public Iterator<Object[]> validUsersFromCsv() throws IOException {
         List<Object[]> list = new ArrayList<Object[]>();
         BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/users.csv")));
         String line = reader.readLine();
@@ -38,11 +40,10 @@ public class UserCreationTests extends TestBase {
             line = reader.readLine();
         }
         return list.iterator();
-    }*/
+    }
 
-    //xml
     @DataProvider
-    public Iterator<Object[]> validUsers() throws IOException {
+    public Iterator<Object[]> validUsersFromXml() throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/users.xml")));
         String xml = "";
         String line = reader.readLine();
@@ -53,10 +54,24 @@ public class UserCreationTests extends TestBase {
         XStream xstream = new XStream();
         xstream.processAnnotations(UserData.class);
         List <UserData> users = (List<UserData>) xstream.fromXML(xml);
-        return users.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+        return users.stream().map((u) -> new Object[] {u}).collect(Collectors.toList()).iterator();
     }
 
-    @Test(dataProvider = "validUsers")
+    @DataProvider
+    public Iterator<Object[]> validUsersFromJson() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/users.json")));
+        String json = "";
+        String line = reader.readLine();
+        while (line != null) {
+            json += line;
+            line = reader.readLine();
+        }
+        Gson gson = new Gson();
+        List <UserData> users = gson.fromJson(json, new TypeToken<List<UserData>>(){}.getType());
+        return users.stream().map((u) -> new Object[] {u}).collect(Collectors.toList()).iterator();
+    }
+
+    @Test(dataProvider = "validUsersFromJson")
     public void testUserCreation(UserData user) throws Exception {
 //        File photo = new File("src/test/resources/goose.jpg");
 //        UserData user =
