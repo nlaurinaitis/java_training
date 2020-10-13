f07feea0407ecf78bba36ace210586dcfd22cb29
Anastasiia Slobodskaia <anastasiia.slobodskaia@t-systems.com>
2020-10-13 23:20:21 +0300

    draft of registration test

--- mantis-tests/src/test/resources/config_inc.php
+++ mantis-tests/src/test/resources/config_inc.php
@@ -10,3 +10,6 @@ $g_default_timezone       = 'Europe/Berlin';
 $g_crypto_master_salt     = 'W0SFihPp3YEn1eWZGhvARE2BgafQM4DXiX1DNsyUN9Q=';
 
 $g_signup_use_captcha = OFF;
+
+$g_phpMailer_method = PHPMAILER_METHOD_SMTP;
+$g_smtp_host = 'localhost';
